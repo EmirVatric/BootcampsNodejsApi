ed609387f3ce7204c1e1b9a6d5b03f4f56579d4a
EmirVatric <vatricemir@gmail.com>
2019-10-21 15:48:01 +0200

    mongoose

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const colors = require('colors')
+const connectDB = require('./config/db')
 
 //load env vars
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: './config/config.env' });
+
+//connecto to database
+connectDB()
 
 const app = express();
 
@@ -24,6 +29,15 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,
-  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+const server = app.listen(PORT,
+  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit
+  server.close(() => {
+    process.exit(1)
+  })
+})
