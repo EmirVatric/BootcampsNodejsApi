5bce194be452f50339a3f9856ac60399ec11d072
EmirVatric <vatricemir@gmail.com>
2019-10-21 14:05:19 +0200

    middleware

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 
 //load env vars
 dotenv.config({ path: './config/config.env' })
 
 const app = express();
 
+//middleware
+
+//dev loggin middleware
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'))
+}
+
 //routes
 const bootcamps = require('./routes/bootcamps')
 
 //Mount routes
 app.use('/api/v1/bootcamps', bootcamps)
+
 
 
 
