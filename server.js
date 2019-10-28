3993fccb24f328f74370d6ce9907878d428e4597
EmirVatric <vatricemir@gmail.com>
2019-10-28 20:15:36 +0100

    code refactoring

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const colors = require('colors')
+const fileupload = require('express-fileupload')
 const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 
 //load env vars
-dotenv.config({ path: './config/config.env' });
+dotenv.config({
+  path: './config/config.env'
+});
 
 //connecto to database
 connectDB()
+
+
 //routes
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
@@ -23,6 +29,12 @@ app.use(express.json())
 if (process.env.NODE_ENV == 'development') {
   app.use(morgan('dev'))
 }
+
+// File uploading
+app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Mount routes
 app.use('/api/v1/bootcamps', bootcamps);
