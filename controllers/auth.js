dab59c8da55a062bbd4ca841c433147c067a5d41
EmirVatric <vatricemir@gmail.com>
2019-11-06 23:55:40 +0100

    auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const User = require("../models/User");
 
-// @desc:     Register user
+// @desc:     Register new user
 // @route:    GET /api/v1/auth/register
 // @access:   Public
 exports.register = asyncHandler(async (req, res, next) => {
