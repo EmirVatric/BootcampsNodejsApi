b73aeb9bbae98ecb3571bc077016e8380baa2072
EmirVatric <vatricemir@gmail.com>
2019-11-08 13:42:02 +0100

    fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -22,7 +22,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
   }
 
   try {
-    // verify token
+    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
     req.user = await User.findById(decoded.id)
