4cca2c0f3ebd22a88b5cf63d1940f8183989b3f4
EmirVatric <vatricemir@gmail.com>
2019-12-28 15:41:23 +0100

    xss

--- server.js
+++ server.js
@@ -1,72 +1,83 @@
-const path = require('path')
-const express = require('express')
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-const colors = require('colors')
-const mongoSanitize = require('express-mongo-sanitize')
-const fileupload = require('express-fileupload')
-const cookieParser = require('cookie-parser')
-const errorHandler = require('./middleware/error')
-const connectDB = require('./config/db')
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const fileupload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
 
 //load env vars
 dotenv.config({
-  path: './config/config.env'
+  path: "./config/config.env"
 });
 
 //connecto to database
-connectDB()
-
+connectDB();
 
 //routes
-const bootcamps = require('./routes/bootcamps');
-const courses = require('./routes/courses');
-const auth = require('./routes/auth')
-const users = require('./routes/users')
-const reviews = require('./routes/reviews')
+const bootcamps = require("./routes/bootcamps");
+const courses = require("./routes/courses");
+const auth = require("./routes/auth");
+const users = require("./routes/users");
+const reviews = require("./routes/reviews");
 
 const app = express();
 
 //body parser
-app.use(express.json())
+app.use(express.json());
 
 // Cookie parser
-app.use(cookieParser())
+app.use(cookieParser());
 
 //dev logger middleware
-if (process.env.NODE_ENV == 'development') {
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV == "development") {
+  app.use(morgan("dev"));
 }
 
 // File uploading
 app.use(fileupload());
 
 // Sanitize data
-app.use(mongoSanitize())
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS
+app.use(xss());
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 //Mount routes
-app.use('/api/v1/bootcamps', bootcamps);
-app.use('/api/v1/courses', courses);
-app.use('/api/v1/auth', auth)
-app.use('/api/v1/users', users)
-app.use('/api/v1/reviews', reviews);
+app.use("/api/v1/bootcamps", bootcamps);
+app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/users", users);
+app.use("/api/v1/reviews", reviews);
 
 //error handler
-app.use(errorHandler)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT,
-  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
 );
 
 // handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
+process.on("unhandledRejection", (err, promise) => {
   console.log(`Error: ${err.message}`.red);
   // Close server and exit
   server.close(() => {
-    process.exit(1)
-  })
-})
+    process.exit(1);
+  });
+});
